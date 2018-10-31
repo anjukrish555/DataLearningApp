020c8ba19271d249ade35416e714983683247bbf
prathikkotian <39869806+prathikkotian@users.noreply.github.com>
2018-10-31 10:16:17 -0400

    changes to package.json adn app.js router logic for viewCourses

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var engine = require('ejs-mate');
 
-var indexRouter = require('./routes/index');
+var viewCoursesRouter = require('./routes/viewCourses');
 var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('ejs',engine);
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/viewCourses', viewCoursesRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
