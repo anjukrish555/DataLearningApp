baecfcdd600b2725fd27314d78def8b9726589df
KavyaSingaravel <kavya.singaravel@yahoo.com>
2018-11-04 19:54:25 -0500

    Updated connection with DB and written scripts to fetch courses

---
+++ routes/viewCourses.js
@@ -0,0 +1,24 @@
+var express= require('express');
+var router = express.Router();
+var connection = require('./connection.js');
+
+router.get('/',function(req, res) {
+    connection.getConnection(function (err, connection) {
+        if (err) throw err
+        var sql = 'select courseID,courseName,city,season,price,classSize,facultyName,socialMediaNotification,videoLinks from classOfferings, faculty where faculty.facultyID=classOfferings.facultyID;';
+
+        connection.query(sql, function (err, rows, fields) {
+            if (err) throw err
+            if (rows != undefined) {
+                var jsonStr = JSON.stringify(rows);
+                var jsonObj = JSON.parse(jsonStr);
+                console.log(jsonObj);
+                res.render('viewCourses',{ title: 'Courses Offered',header: true, navbar: true, courses:jsonObj});
+            }
+            connection.release();
+        });
+    });
+});
+module.exports = router;
+
+
