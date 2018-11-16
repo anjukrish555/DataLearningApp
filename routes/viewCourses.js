8dbd5fce08aedb728dcc2c8b3da2a2c6e713dcc3
prathikkotian <39869806+prathikkotian@users.noreply.github.com>
2018-11-16 11:38:39 -0500

    fixed merge conflicts

--- routes/viewCourses.js
+++ routes/viewCourses.js
@@ -2,31 +2,10 @@ var express= require('express');
 //var mysql = require('mysql');
 var router = express.Router();
 var connection = require('./connection.js');
-
-/* var connection = mysql.createConnection({
-    connectionLimit : 100,
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'ddse'
-
-})
- var sql = 'select courseID,courseName,city,season,price,classSize,facultyName,facultyDescription,socialMediaNotification,videoLinks from classOfferings, faculty where faculty.facultyID=classOfferings.facultyID;';
-
-       connection.query(sql, function (err, rows, fields) {
-            if (err) throw err
-            if (rows != undefined) {
-                var jsonStr = JSON.stringify(rows);
-                var jsonObj = JSON.parse(jsonStr);
-                console.log(jsonObj);
-                res.render('viewCourses',{ title: 'Courses Offered',header: true, navbar: true, courses:jsonObj});
-            }
-            connection.release();
-        }); */
 router.get('/',function(req, res) {
     connection.getConnection(function (err, connection) {
         if (err) throw err
-        var sql = 'select courseID,courseName,city,season,price,classSize,facultyName,facultyDescription,socialMediaNotification,videoLinks from classOfferings, faculty where faculty.facultyID=classOfferings.facultyID;';
+        var sql = 'select courseID,courseName,city,season,price,classSize,facultyName,facultyQualification,facultyDescription,socialMediaNotification,videoLinks from classOfferings, faculty where faculty.facultyID=classOfferings.facultyID;';
 
         connection.query(sql, function (err, rows, fields) {
             if (err) throw err
@@ -34,7 +13,7 @@ router.get('/',function(req, res) {
                 var jsonStr = JSON.stringify(rows);
                 var jsonObj = JSON.parse(jsonStr);
                 console.log(jsonObj);
-                res.render('viewCourses',{ title: 'Courses Offered',header: true, navbar: true, courses:jsonObj});
+                res.render('viewCourses',{ title: 'Courses Offered',header: true, navbar: true, cart: true, signUp: true, courses:jsonObj});
             }
             connection.release();
         });
