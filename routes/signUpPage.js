ebffba21296b419ade8c00c5be02d59e7a6b696f
KavyaSingaravel <kavya.singaravel@yahoo.com>
2018-11-15 20:13:13 -0500

    Updated changes as per comments

--- routes/signUpPage.js
+++ routes/signUpPage.js
@@ -5,23 +5,25 @@ var connection = require('./connection.js');
 router.get('/',function (req,res,next) {
     res.render('signUp',{ title: 'Sign Up',header: true, navbar: true, signUp: false, cart: false});
 });
-router.post(,function(req,res,next){
+router.post('/signup',function(req,res,next){
 	connection.getConnection(function (err, connection) {
 		if (err) throw err
 	if(!req.body.fullname || !req.body.email ||!req.body.password ||!req.body.contact ||!req.body.background){
 		res.json({'error':'THe input field/s is empty'});
 	}else{
-		var sql_stud='INSERT INTO student(studentName,studentAcademicbg,studentContactDetails) VALUES (?,?,?)',[req.body.fullname,req.body.background,req.body.contact];
-		var sql_login='INSERT INTO login(emailID,password) VALUES (?,?)'[req.body.email,!req.body.password];
+		//var sql_stud='INSERT INTO student(studentName,studentAcademicbg,studentContactDetails) VALUES (?,?,?)',[req.body.fullname,req.body.background,req.body.contact];
+		//var sql_login='INSERT INTO login(emailID,password) VALUES (?,?)'[req.body.email,!req.body.password];
 		var k=1;
-		connection.query(sql_stud,function(error,rows,fields){
+		connection.query('INSERT INTO student(studentName,studentAcademicbg,studentContactDetails) VALUES (?,?,?)',[req.body.fullname,req.body.background,req.body.contact],
+		function(error,rows,fields){
 		if (err) throw err
 		else{
 			k++;
 			//res.render('message':req.body.fullname+' was registered successfully')
 		}
 		});
-		connection.query(sql_login,function(error,rows,fields){
+		connection.query('INSERT INTO login(emailID,password) VALUES (?,?)'[req.body.email,!req.body.password],		
+		function(error,rows,fields){
 		if (err) throw err
 		else{
 			k++;
@@ -29,7 +31,8 @@ router.post(,function(req,res,next){
 		}
 		});
 		if(k==2)
-			res.render('message':req.body.fullname+' was registered successfully')
+			 res.render('signUp',{ title: 'Sign Up',header: true, navbar: true, signUp: false, cart: false});
+			//res.render('message',{req.body.fullname+' was registered successfully'})
 	}
 	 connection.release();
 	});
--- routes/viewCourses.js
+++ routes/viewCourses.js
@@ -1,7 +1,28 @@
 var express= require('express');
+//var mysql = require('mysql');
 var router = express.Router();
 var connection = require('./connection.js');
 
+/* var connection = mysql.createConnection({
+    connectionLimit : 100,
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'ddse'
+
+})
+ var sql = 'select courseID,courseName,city,season,price,classSize,facultyName,facultyDescription,socialMediaNotification,videoLinks from classOfferings, faculty where faculty.facultyID=classOfferings.facultyID;';
+
+       connection.query(sql, function (err, rows, fields) {
+            if (err) throw err
+            if (rows != undefined) {
+                var jsonStr = JSON.stringify(rows);
+                var jsonObj = JSON.parse(jsonStr);
+                console.log(jsonObj);
+                res.render('viewCourses',{ title: 'Courses Offered',header: true, navbar: true, courses:jsonObj});
+            }
+            connection.release();
+        }); */
 router.get('/',function(req, res) {
     connection.getConnection(function (err, connection) {
         if (err) throw err
