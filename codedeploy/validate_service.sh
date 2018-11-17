a2d966e52e02edeb6157421d4aaec834242e94c5
anjukrish555 <39890824+anjukrish555@users.noreply.github.com>
2018-11-16 19:06:23 -0500

    Update validate_service.sh

--- codedeploy/validate_service.sh
+++ codedeploy/validate_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is used to validate application 
-ipaddr= http://ec2-18-218-222-63.us-east-2.compute.amazonaws.com:5000
+ipaddr= $(curl http://ec2-18-218-222-63.us-east-2.compute.amazonaws.com:5000/)
 listencount=$(netstat -an | grep 5000 | grep LISTEN | wc -l)
 if [ "$listencount" -lt 1 ]; then
     exit 1
