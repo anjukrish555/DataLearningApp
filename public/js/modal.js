3b689b5e85f611f491ba94b25fe7acbdb46aadd8
prathikkotian <39869806+prathikkotian@users.noreply.github.com>
2018-11-19 01:11:01 -0500

    minor ui changes

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,6 +1,8 @@
 $('#myModal').on('show.bs.modal', function(e) {
     var yourparameter1 = e.relatedTarget.dataset.yourparameter1;
     var yourparameter2 = e.relatedTarget.dataset.yourparameter2;
+    var yourparameter3 = e.relatedTarget.dataset.yourparameter3;
     $(".modal-title #facultyName").html(yourparameter1);
     $(".modal-body #qualification").html(yourparameter2);
+    $(".modal-body #description").html(yourparameter3);
 });
